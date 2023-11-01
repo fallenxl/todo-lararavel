16c1aa7697cef255a9e6ea1de7889bb35a77d6d8
FallEn <axlsntz.dev@gmail.com>
2023-11-01 17:29:16 -0600

    update skeleton color

--- resources/js/vue/components/TasksSkeleton.vue
+++ resources/js/vue/components/TasksSkeleton.vue
@@ -1,15 +1,15 @@
 <template>
-  <li v-for="index in count" :key="index" class="py-4 px-6 bg-gray-700 rounded shadow animate-pulse duration-300">
+  <li v-for="index in count" :key="index" class="py-4 px-6 bg-[#161b22] rounded shadow animate-pulse duration-300">
     <div class="flex items-center justify-between">
       <div class="flex items-center flex-grow gap-4">
         <div  class="flex flex-col flex-grow gap-2 cursor-pointer">
-          <div class="h-4 bg-gray-600 rounded w-1/2"></div>
-          <div class="h-4 bg-gray-600 rounded w-1/4"></div>
+          <div class="h-4 bg-gray-700 rounded w-1/2"></div>
+          <div class="h-4 bg-gray-700 rounded w-1/4"></div>
         </div>
       </div>
       <div class="flex items-center gap-4">
-        <div class="h-4 bg-gray-600 rounded w-1/4 animate-pulse"></div>
-        <div class="h-4 bg-gray-600 rounded w-1/4"></div>
+        <div class="h-4 bg-gray-700 rounded w-1/4 animate-pulse"></div>
+        <div class="h-4 bg-gray-700 rounded w-1/4"></div>
       </div>
     </div>
   </li>
